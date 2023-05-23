98451d51bf12eefa783ee9330f61d29d543c0914
irinamihai150 <irinamihai150@gmail.com>
2023-05-23 21:30:20 +0100

    implementd update user

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { Form, Button, Row, Col } from "react-bootstrap"
 import Message from "../components/Message"
 import Loader from "../components/Loader"
-import { getUserDetails } from "../actions/userActions"
+import { getUserDetails, updateUserProfile } from "../actions/userActions"
 import { useNavigate } from "react-router-dom"
 
 function ProfileScreen() {
@@ -21,6 +21,8 @@ function ProfileScreen() {
 	const userLogin = useSelector((state) => state.userLogin)
 	const { userInfo } = userLogin
 
+	const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
+	const { success } = userUpdateProfile
 	useEffect(() => {
 		if (!userInfo) {
 			navigate("/login")
@@ -40,7 +42,7 @@ function ProfileScreen() {
 		if (password !== confirmPassword) {
 			setMessage("Passwords do not match")
 		} else {
-			//dispatch update profile
+			dispatch(updateUserProfile({ id: user._id, name, email, password }))
 		}
 	}
 	return (
@@ -49,6 +51,7 @@ function ProfileScreen() {
 				<h2>User Profile</h2>
 				{message && <Message variant='danger'>{message}</Message>}
 				{error && <Message variant='danger'>{error}</Message>}
+				{success && <Message variant='success'>Profile Updated</Message>}
 				{loading && <Loader />}
 				<Form onSubmit={submitHandler}>
 					<Form.Group controlId='name'>

91bde800ddcc4612814c4f743f6859adf3400e0f
irinamihai150 <irinamihai150@gmail.com>
2023-05-25 16:19:29 +0100

    add checkoutsteps and shipingscreen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -4,6 +4,7 @@ import { Form, Button } from "react-bootstrap"
 import FormContainer from "../components/FormContainer.js"
 import { saveShippingAddress } from "../actions/cartActions.js"
 import { useNavigate } from "react-router-dom"
+import CheckoutSteps from "../components/CheckoutSteps.js"
 
 const ShippingScreen = () => {
 	const cart = useSelector((state) => state.cart)
@@ -24,6 +25,7 @@ const ShippingScreen = () => {
 	}
 	return (
 		<FormContainer>
+			<CheckoutSteps step1 step2 />
 			<h1>Shipping</h1>
 			<Form onSubmit={submitHandler}>
 				<Form.Group controlId='address'>
